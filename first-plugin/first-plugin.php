c62c30b333f23ae435fa92359f52047c20337e00
Shams Arefin Khan <robeeen@gmail.com>
2022-01-25 22:32:31 +0600

    more updates

--- first-plugin/first-plugin.php
+++ first-plugin/first-plugin.php
@@ -22,14 +22,21 @@
 class wordCount {
 	function __construct(){
 		add_action('admin_menu', array($this, 'adminPage'));
+		add_action('admin_init', array($this, 'settings'));
 	}
 
-	function adminPage(){
+	function settings () {
+		add_settings_field('wcp_location', 'Displa Location', array($this, 'locationHTML'), 'word-count-settings', 'first_section');
+		register_setting('wordCountPlugin', 'wcp_location', array('sanitize_callback'=>'sanitize_text_field', 'default' => '0'));
+		
+		function adminPage(){
 		add_options_page('Word Count Settings', 'Word Count', 'manage_options', 'word-count-settings', array($this, 'ourHTML'));
 	}
 
 	function ourHTML(){ ?>
-		Hi this is HTML page
+		<div class="wrap">
+			<h1>Word Count Settings:</h1>
+		</div>
 	<?php
 	}
 }
