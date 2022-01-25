958ef6af1ba93ba0cbfd91873636a2937fdd10b4
Shams Arefin Khan <robeeen@gmail.com>
2022-01-25 22:08:17 +0600

    Added Class, consts

--- first-plugin/first-plugin.php
+++ first-plugin/first-plugin.php
@@ -7,15 +7,35 @@
 	Author URL: https://shamskhan.xyz
 */
 
-	add_filter('the_content', 'addToEndOfPost');
+	// add_filter('the_content', 'addToEndOfPost');
 
-	//This plugin will add h3 headline just below content of the page is_page() only.
+	// //This plugin will add h3 headline just below content of the page is_page() only.
 
-	function addToEndOfPost($content){
-		if( is_page() && is_main_query()){
-			return $content . '<h3>This is my Name - Shams</h3>';
-		}
+	// function addToEndOfPost($content){
+	// 	if( is_page() && is_main_query()){
+	// 		return $content . '<h3>This is my Name - Shams</h3>';
+	// 	}
 		
-		return $content;
+	// 	return $content;
+	// }
+
+class wordCount {
+	function __construct(){
+		add_action('admin_menu', array($this, 'adminPage'));
 	}
 
+	function adminPage(){
+		add_options_page('Word Count Settings', 'Word Count', 'manage_options', 'word-count-settings', array($this, 'ourHTML'));
+	}
+
+	function ourHTML(){ ?>
+		Hi this is HTML page
+	<?php
+	}
+}
+
+$wordCount = new wordCount();
+
+	
+
+	
